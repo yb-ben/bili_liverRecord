bb3a111b457fea3b5a0a67c8b679d598a178c2bb
huyibin <804141892@qq.com>
2020-05-04 10:28:52 +0800

    save

--- run.php
+++ run.php
@@ -10,16 +10,17 @@ require './vendor/autoload.php';
 
 $pid = \core\Process::daemon();
 
-
-if(!isset($argv[1])){
-    die('please input the liver room id');
-}
-$roomId = $argv[1];
-
 $loggerConfig = require './logger.php';
 $path = $loggerConfig['channels'][$loggerConfig['default']]['path'];
 $logger =new Logger($path);
 $logger->pushHandler(new StreamHandler($path));
+
+
+if(!isset($argv[1])){
+    $logger->debug('please input the room id');
+    exit;
+}
+$roomId = $argv[1];
 
 
 $recorder = new LiverRecorder();
