055ae8e48a619979af1e95cb2017652fb86d8dc2
huyibin <804141892@qq.com>
2020-11-04 21:47:11 +0800

    save

--- run.php
+++ run.php
@@ -6,18 +6,16 @@ use Swoole\Process;
 
 require './vendor/autoload.php';
 define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
-
 $logConfig = require 'config/log.php';
-
-
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
-
-
 
 isset($argv[1]) || die('please input the room id');
 
 $roomId = $argv[1];
+
+Process::daemon(true,false);
+
 
 
 $channelConfig = $logConfig['channels'][$logConfig['default']];
@@ -29,6 +27,14 @@ $logger->pushHandler($stream);
 $recorder = new LiverRecorder();
 $recorder->setLogger($logger);
 
+$pid = null;
+
+Process::signal(15,function($signo)use($logger,&$pid){
+    $logger->info('[SIG]'.$signo);
+    Process::kill($pid);
+    exit;
+});
+
 while(true){
     $process = new Process(function ()use($recorder,$roomId,$logger) {
         $logger->info('My pid:'.getmypid());
@@ -39,7 +45,7 @@ while(true){
         }
     });
 
-    $process->start();
+    $pid  = $process->start();
     $status = Process::wait(true);
     $logger->info("Recycled #{$status['pid']}, code={$status['code']}, signal={$status['signal']}" . PHP_EOL);
 }
