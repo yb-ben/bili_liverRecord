b9b91c286bf05c9d2fc471b112ac738fa434de50
huyibin <804141892@qq.com>
2020-11-05 21:57:40 +0800

    save

--- watch.sh
+++ watch.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-cd ./pid
+rootpath=/usr/local/bili_liverRecord
+pidpath=$rootpath/pid
 
-for i in `ls`;
+if [ ! -d $pidpath ];then
+mkdir $pidpath
+fi
+
+cd $rootpath
+
+for i in `ls $pidpath`;
 do
 
 count=`ps -ef | grep $i | grep -v "grep" | wc -l`
 
-if [ 0 -eq $count ]
+if [ 0 -eq $count ];
 then
-  php run.php $i --daemon
+  /usr/local/bin/php run.php $i --daemon
 fi
 done
+
+echo  `date "+%Y-%m-%d %H:%M:%S"`
+echo -e "finish a loop"
